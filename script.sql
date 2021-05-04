571ffe289a0cb33ccf6cd4bcefe897ad72364610
davidpique55 <79598361+davidpique55@users.noreply.github.com>
2021-05-04 13:50:22 +0200

    Update script.sql

--- script.sql
+++ script.sql
@@ -18,28 +18,22 @@ GRANT 'programmer' TO 'pique'@'localhost';
 GRANT 'final_user' TO 'marin'@'localhost';
 
 CREATE TABLE pique_alumnos (
- Codigo_Postal INT,
- Ciudad VARCHAR(50),
- Provincia VARCHAR(20),
- Pais VARCHAR(20),
- PRIMARY KEY (Codigo_Postal)
+ DNI CHAR(10) ,
+ Nombre VARCHAR(20),
+ Apellidos VARCHAR(20),
+ PRIMARY KEY (DNI)
 );
 
 CREATE TABLE pique_asignaturas (
-Id_Tipo_Mascota INT,
-Tipo_Mascota VARCHAR(20) NOT NULL,
-PRIMARY KEY (Id_Tipo_Mascota)
+ Codigo CHAR(10),
+ Asignatura VARCHAR(100),
+ PRIMARY KEY (Codigo)
 );
 
-CREATE TABLE G1_calificaciones (
- Id_Direccion INT,
- Tipo_via VARCHAR(10),
- Calle VARCHAR(20),
- Numero INT,
- Escalera VARCHAR(10),
- Piso INT,
- Puerta VARCHAR(2),
- Codigo_Postal INT,
- PRIMARY KEY (Id_Direccion),
- FOREIGN KEY (Codigo_Postal) REFERENCES G1_codigo_postal(Codigo_Postal)    
+CREATE TABLE pique_calificaciones (
+ Nota  INT,
+ ID_Calificacion INT,
+ FOREIGN KEY (DNI) REFERENCES pique_alumnos(DNI),
+ FOREIGN KEY (Codigo) REFERENCES pique_asignaturas(Codigo), 
+ PRIMARY KEY (ID_Calificacion)
 );
